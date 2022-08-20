beb56ee9f3d161f15b646e3d6e4cbcc5cd077c5b
Jones Prabu <jonesprabu@gmail.com>
2022-08-21 01:39:13 +0530

    initial commit

---
+++ apps/our/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Message } from '@edify-technos/api-interfaces';
+
+@Component({
+  selector: 'edify-technos-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  hello$ = this.http.get<Message>('/api/hello');
+  constructor(private http: HttpClient) {}
+}
